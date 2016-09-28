98394dd072cbece4244a82f673ff6f8107470a62
Jonas <jonas.baech@gmail.com>
2016-09-28 16:36:56 +0200

    weatherService mm

--- Assignment_2/app/src/main/java/mathiassiig/assignment2/MainActivity.java
+++ Assignment_2/app/src/main/java/mathiassiig/assignment2/MainActivity.java
@@ -1,13 +1,64 @@
 package mathiassiig.assignment2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "d5a8341b52c8adfc0b4ec902bf53261c"; //Jonas API
+    private static final long ID_CITY = 2624652; //Aarhus
+    private static final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/forecast/city?id=" + ID_CITY + "&APPID=" + API_KEY;
+
+    private Button checkConn;
+    private Button getWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkConn = (Button)findViewById(R.id.buttonCheck);
+        getWeather = (Button)findViewById(R.id.buttonWeather);
+
+        checkConn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkConnection();
+            }
+        });
+
+        getWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weatherRequest();
+            }
+        });
+    }
+
+    private void weatherRequest() {
+        Intent backgroundServiceIntent = new Intent(MainActivity.this, weatherService.class);
+        backgroundServiceIntent.putExtra("weather", WEATHER_URL);
+        startService(backgroundServiceIntent);
+
+    }
+
+    private boolean checkConnection() {
+        ConnectivityManager connectionManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Toast.makeText(MainActivity.this, "You are connected " +
+                    networkInfo.toString(), Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            Toast.makeText(MainActivity.this, "No connection " + networkInfo.toString(), Toast.LENGTH_SHORT).show();
+            return false;
+        }
     }
 }
---
+++ Assignment_2/app/src/main/java/mathiassiig/assignment2/weatherService.java
@@ -0,0 +1,151 @@
+package mathiassiig.assignment2;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class weatherService extends Service {
+    public weatherService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String weatherURL = intent.getStringExtra("weather");
+        runInBackground(weatherURL);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void runInBackground(final String weatherURL) {
+
+
+
+        AsyncTask<Object, Object, String> task = new AsyncTask<Object, Object, String>() {
+            @Override
+            protected String doInBackground(Object[] params) {
+                String weatherData;
+                String s = "Background job";
+                try {
+                   // Thread.sleep(10000);
+                    weatherData = callURL(weatherURL);
+                } catch (Exception e) {
+                    s+= " did not finish due to error";
+                    return s;
+                }
+
+             //   Toast.makeText(weatherService.this, "Weather:" +weatherData.toString(), Toast.LENGTH_SHORT).show();
+
+                return weatherData;
+            }
+
+
+            @Override
+            protected void onPostExecute(String stringResult) {
+                super.onPostExecute(stringResult);
+            //    broadcastTaskResult(stringResult);
+
+                //if Service is still running, keep doing this recursively
+             //   if(started){
+             //       doBackgroundThing(waitTimeInMilis);
+             //   }
+            }
+        };
+
+        task.execute();
+
+    }
+
+    private String callURL(String callUrl) {
+
+        InputStream is = null;
+
+        try {
+            //create URL
+            URL url = new URL(callUrl);
+
+            //configure HttpURLConnetion object
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+
+
+            // Starts the request
+            conn.connect();
+            int response = conn.getResponseCode();
+
+            //probably check check on response code here!
+
+            //give user feedback in case of error
+
+
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+
+            String contentAsString = convertStreamToStringBuffered(is);
+            return contentAsString;
+
+
+        } catch (ProtocolException pe) {
+
+        } catch (UnsupportedEncodingException uee) {
+
+        } catch (IOException ioe) {
+
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException ioe) {
+
+                }
+            }
+        }
+        return null;
+    }
+
+    private String convertStreamToStringBuffered(InputStream is) {
+        String s = "";
+        String line = "";
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+
+
+        try {
+            while ((line = rd.readLine()) != null) { s += line; }
+        } catch (IOException ex) {
+           ;
+        }
+
+        // Return full string
+        return s;
+    }
+
+    private void broadcastTaskResult(String result){
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.putExtra("Result", result);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent);
+    }
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
