9e97b1f6ef4a47f661f7a9ce08fd5ea28e3d1857
Mathias <mathias_sn@hotmail.com>
2016-10-06 11:18:46 +0200

    Database settings er go

---
+++ RoomienatorWeb/RoomienatorWeb/Models/GroceryItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RoomienatorWeb.Models
+{
+    public class GroceryItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public DateTime Creation { get; set; }
+        public DateTime Bought { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int ApartmentID { get; set; }
+        public Apartment Apartment { get; set; }
+    }
+}
---
+++ RoomienatorWeb/RoomienatorWeb/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RoomienatorWeb.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Pass { get; set; }
+        public byte[] Avatar { get; set; }
+
+    }
+}
