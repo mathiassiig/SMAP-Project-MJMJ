1d3135ee67642313e16d582ddd11b6af565364c2
jeppemalm <jeppe_Malm@hotmail.com>
2016-10-11 20:51:08 +0200

    tilføjet notifikation service og reciever

---
+++ Eksamensopgaven/ExamProject/app/src/main/java/examproject/group22/roominator/NotifikationReceiver.java
@@ -0,0 +1,17 @@
+package examproject.group22.roominator;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotifikationReceiver extends BroadcastReceiver {
+    public NotifikationReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
